3ad93d2d2931d0cee07c654a0240dc61e1380b0a
kiwon-k <kiwon.k@navercorp.com>
2022-12-29 12:47:04 +0900

    feat: 1강 Observer 패턴과 Reactive Streams

---
+++ src/main/java/Ob.java
@@ -0,0 +1,42 @@
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SuppressWarnings("deprecation")
+public class Ob {
+
+    // 데이터를 발행하는 주체. Observable 별도 스레드로 구성.
+    static class IntObservable extends Observable implements Runnable {
+        @Override
+        public void run() {
+            for(int i=1;i <=10; i++) {
+                setChanged();
+                notifyObservers(i);     // push
+                // int i = it.next();   // pull
+            }
+        }
+    }
+
+    // 기존은 이미 존재하는 데이터를 땡겨오는 방식 > pull
+    // 옵저버 패턴은 데이터를 발행하고 넘겨주는 방식 > push
+    //  push 방식은 발행과 수신해서 사용하는 스레드를. 별개의 스레드에서 동작하도록 매우 손쉽게 만들 수 있다.
+    public static void main(String[] args) {
+        // 데이터를 구독하는 주체. Observer
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(Thread.currentThread().getName() + " " + arg);
+            }
+        };
+
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(io);
+
+        System.out.println(Thread.currentThread().getName() + " EXIT");
+        es.shutdown();
+    }
+}
---
+++ src/main/java/PubSub.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.TimeUnit;
+
+public class PubSub {
+
+    public static void main(String[] args) throws InterruptedException {
+        Iterable<Integer> itr = Arrays.asList(1,2,3,4,5);
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Publisher p = new Publisher() {
+            @Override
+            public void subscribe(Subscriber subscriber) {
+                Iterator<Integer> it = itr.iterator();
+
+                // Subscription의 request를 구현해서 최초구독시 넘겨줌
+                subscriber.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        es.execute(() -> {
+                            int i = 0;
+                            try {
+                                while(i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    }
+                                    else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
+                                }
+                            } catch(RuntimeException e) {
+                                subscriber.onError(e);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> s = new Subscriber<Integer>() {
+            // 최초 연동시 onSubscribe 호출될때 받은 Subscription onNext에서도 사용하기위해 필드로 저장
+            Subscription subscription;
+            int bufferSize = 2;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println(Thread.currentThread().getName() + " onSubscribe");
+                this.subscription = subscription;
+                subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println(Thread.currentThread().getName() + " onNext" + item);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println(Thread.currentThread().getName() + " onComplete");
+            }
+        };
+
+        p.subscribe(s);
+
+        es.awaitTermination(10, TimeUnit.HOURS);
+        es.shutdown();
+    }
+}
